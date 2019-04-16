a6715b1a605b2a45ec286fe9bbaeb841b59a6d0b
Jonathan Scott <scottjonathan@google.com>
2019-04-16 23:50:15 +0100

    add drawing interface

--- client/client.ts
+++ client/client.ts
@@ -35,6 +35,10 @@ function beginDrawingBody() {
 	$("#creature-creator-stage-description").text("This should not include a head, arms, or legs. They will be drawn later.");
 	$("#body-controls").hide();
 	$("#draw-body-controls").show();
+	$("#body-canvas").show();
+	$("#body-canvas").bind("click", function() {
+		alert("A");
+	})
 }
 
 function backToBodySelector() {
@@ -42,11 +46,48 @@ function backToBodySelector() {
 	$("#creature-creator-stage-description").text("");
 	$("#body-controls").show();
 	$("#draw-body-controls").hide();
+	$("#body-canvas").hide();
+	$("#body-canvas").unbind("click")
+}
+
+$(".tool").bind("click", function() {
+	state.creature_creator.selected_tool = $(this).data("tool");
+	updateCreatureCreatorDrawingBody();
+});
+
+$(".color").bind("click", function() {
+	state.creature_creator.selected_color = $(this).data("color");
+	updateCreatureCreatorDrawingBody();
+});
+
+
+function updateCreatureCreatorDrawingBody() {
+	$(".tool").removeClass("selected");
+	$(".tool[data-tool='" + state.creature_creator.selected_tool + "']").addClass("selected");
+
+	$(".color").removeClass("selected");
+	$(".color[data-color='" + state.creature_creator.selected_color + "']").addClass("selected");
 }
 
 
 var state = {
 	creature_creator: {
-		body_type: 0
+		body_type: 0,
+		selected_tool: "pencil",
+		selected_color: "000"
 	}
 };
+
+var tools = {
+	pencil: function(ctx, x, y, color) {
+
+	},
+
+	bucket: function(ctx, x, y, color) {
+
+	},
+
+	eraser: function(ctx, x, y, color) {
+		
+	}
+}
